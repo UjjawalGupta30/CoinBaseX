0b4ac9c3f0094c95fdacf55913d1dcc8486afbdc
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2022-02-24 23:33:22 +0530

    Page setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
 
+const supportedChainIds = [4];
+const connectors = {
+  injected: {},
+};
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <Component {...pageProps} />;
+    </ThirdwebWeb3Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
