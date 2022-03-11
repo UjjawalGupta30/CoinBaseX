64229f142891ba3e3e279d72170c702baed689c0
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2022-03-12 01:21:52 +0530

    Created Promos for website

---
+++ components/Promos.js
@@ -0,0 +1,67 @@
+import React from "react";
+import styled from "styled-components";
+
+const Promos = () => {
+  return (
+    <Wrapper>
+      <OfferCard>
+        <Title>Yield earned</Title>
+        <Description>Earn Up to 2.84% APY on your crypto</Description>
+        <Placeholder />
+        <Additional style={{ fontSize: "1.5rem" }}>
+          $0.000066 <span>2.84% APY</span>
+        </Additional>
+      </OfferCard>
+      <OfferCard>
+        <Title>Learn and Earn</Title>
+        <Description>Earn Up to 35% APY on your crypto</Description>
+        <Placeholder />
+        <Additional style={{ color: "#3773f5" }}>Verify Identity</Additional>
+      </OfferCard>
+    </Wrapper>
+  );
+};
+
+export default Promos;
+
+const Wrapper = styled.div`
+  margin-top: 2rem;
+  padding-right: 3rem;
+`;
+
+const OfferCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 21rem;
+  height: 11rem;
+  padding: 1.5rem;
+  margin-bottom: 1rem;
+  border: 1px solid #282b2f;
+`;
+
+const Title = styled.div`
+  font-weight: 700;
+  font-size: 1.5rem;
+  margin-bottom: 0.1rem;
+`;
+
+const Description = styled.div`
+  font-size: 1.1rem;
+`;
+
+const Placeholder = styled.div`
+  flex: 1;
+`;
+
+const Additional = styled.div`
+  font-size: 1.1rem;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & > span {
+    color: #8a919e !important;
+    font-size: 1rem;
+  }
+`;
