d8c0d9aad53c886f4def25d4f402e5e52499c3d2
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2022-04-05 16:14:14 +0530

    Created Dashboard

--- pages/DashBoard.js
+++ pages/DashBoard.js
@@ -1,16 +1,52 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import styled from "styled-components";
 import Sidebar from "../components/Sidebar";
+import { ethers } from "ethers";
+import { ThirdwebSDK } from "@3rdweb/sdk";
+
+const sdk = new ThirdwebSDK(
+  new ethers.Wallet(
+    process.env.NEXT_PUBLIC_METAMASK_KEY,
+    ethers.getDefaultProvider("https://rinkeby.infura.io/v3/")
+  )
+);
 
 const DashBoard = (address) => {
+  const [sanityTokens, setSanityTokens] = useState([]);
+  const [thirdWebTokens, setThirdWebTokens] = useState([]);
+
+  useEffect(() => {
+    const getSanityAndThirdWebTokens = async () => {
+      const coins = await fetch(
+        "https://twvzz1tl.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D'coins'%5D%7B%0A%20%20name%2C%0A%20%20usdPrice%2C%0A%20%20contractAddress%2C%0A%20%20symbol%2C%0A%20%20logo%0A%7D"
+      );
+      const sanityTokens = (await coins.json()).result;
+      setSanityTokens(sanityTokens);
+
+      setThirdWebTokens(
+        sanityTokens.map((token) => sdk.getTokenModule(token.contractAddress))
+      );
+    };
+
+    return getSanityAndThirdWebTokens();
+  }, []);
+
   return (
     <Wrapper>
       <Sidebar />
       <MainContainer>
-        <Header walletAddress= {address} />
-        <Main />
+        <Header
+          walletAddress={address}
+          sanityTokens={sanityTokens}
+          thirdWebTokens={thirdWebTokens}
+        />
+        <Main
+          walletAddress={address}
+          sanityTokens={sanityTokens}
+          thirdWebTokens={thirdWebTokens}
+        />
       </MainContainer>
     </Wrapper>
   );
@@ -20,8 +56,8 @@ export default DashBoard;
 
 const Wrapper = styled.div`
   display: flex;
-  height: 100vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
   color: white;
   background-color: #0a0b0d;
   overflow: hidden;
