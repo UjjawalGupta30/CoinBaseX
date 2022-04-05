bf1ac9358e409370b828fe18362d76c496a54f5d
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2022-04-05 16:13:27 +0530

    Added portofolio

--- components/Main.js
+++ components/Main.js
@@ -3,10 +3,14 @@ import Portfolio from "./Portfolio";
 import styled from "styled-components";
 import Promos from "./Promos";
 
-const Main = () => {
+const Main = ({ thirdWebTokens, sanityTokens, walletAddress }) => {
   return (
     <Wrapper>
-      <Portfolio />
+      <Portfolio
+        walletAddress={walletAddress}
+        sanityTokens={sanityTokens}
+        thirdWebTokens={thirdWebTokens}
+      />
       <Promos />
     </Wrapper>
   );
@@ -18,6 +22,11 @@ const Wrapper = styled.div`
   display: flex;
   max-height: calc(100vh - 64px);
   overflow: hidden;
+  overflow-y: auto;
+  scrollbar-behavior: smooth;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 
   & div {
     border-radius: 0.4rem;
